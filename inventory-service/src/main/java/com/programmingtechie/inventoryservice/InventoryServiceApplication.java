a5bda07d60e4cef7c013ae32dc5f3dbd2166763c
Roman Moskalenko <2016roman00@gmail.com>
2022-11-22 10:23:16 +0200

    Service discovery using Eureka

--- inventory-service/src/main/java/com/programmingtechie/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/programmingtechie/inventoryservice/InventoryServiceApplication.java
@@ -5,9 +5,11 @@ import com.programmingtechie.inventoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
@@ -18,11 +20,11 @@ public class InventoryServiceApplication {
 	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
 		return args -> {
 			Inventory inventory1 = new Inventory();
-			inventory1.setSkuCode("iphone 13" + System.currentTimeMillis());
+			inventory1.setSkuCode("iphone_13");
 			inventory1.setQuantity(100);
 
 			Inventory inventory2 = new Inventory();
-			inventory2.setSkuCode("iphone 13_2" + System.currentTimeMillis());
+			inventory2.setSkuCode("iphone_13_2");
 			inventory2.setQuantity(200);
 
 			inventoryRepository.save(inventory1);
--- order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
@@ -18,11 +18,11 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
-    public OrderService(OrderRepository orderRepository, WebClient webClient) {
+    public OrderService(OrderRepository orderRepository, WebClient.Builder webClientBuilder) {
         this.orderRepository = orderRepository;
-        this.webClient = webClient;
+        this.webClientBuilder = webClientBuilder;
     }
 
     public void placeOrder(OrderRequest orderRequest) {
@@ -41,8 +41,8 @@ public class OrderService {
 
         // Call inventory service and place order if product is in
 
-        var inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/inventory/",
+        var inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory/",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
